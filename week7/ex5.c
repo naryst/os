7d00c385074b6b95c9c1eacaddbd99d594d12191
Gooxezecyra <nikitasergeev692@gmail.com>
2021-10-05 23:36:03 +0300

    bug fix

--- week7/ex5.c
+++ week7/ex5.c
@@ -2,10 +2,10 @@
 #include<malloc.h>
 
 int main () {
-    char **s = malloc(sizeof (char));
-    char foo [] = " Hello World ";
+    char **s = malloc (sizeof (char));
+    char foo[] = " Hello World ";
     *s = foo;
-    printf ("s is %s\n", s);
+    printf ("s is %s\n", *s);
     s[0] = foo;
     printf ( "s[0] is %s \n", s[0]) ;
     return (0) ;
